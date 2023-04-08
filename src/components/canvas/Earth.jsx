731f61cd3c05b2d3f077b93e4a7993f7d0d20137
Vulquimar Silva <vuquimar@hotmail.com>
2023-04-08 15:19:06 -0300

    feat: I changed 3d model to earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,70 +1,43 @@
-import { Preload } from "@react-three/drei";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import React, { Suspense, useRef } from "react";
-import { TextureLoader } from "three";
-import * as THREE from "three";
+import { OrbitControls, Preload, useAnimations, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
 
 import CanvasLoader from "../Loader";
 
-import EarthDayMap from "/earth/textures/8k_earth_daymap.jpg";
-import EarthNormalMap from "/earth/textures/8k_earth_normal_map.jpg";
-import EarthSpecularMap from "/earth/textures/8k_earth_specular_map.jpg";
-import EarthCloudsMap from "/earth/textures/8k_earth_clouds.jpg";
-
 const Earth = () => {
 
-  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
-    TextureLoader,
-    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
-  );
-
-  const earthRef = useRef();
-  const cloudsRef = useRef();
-
-  useFrame(({ clock }) => {
-    const elapsedTime = clock.getElapsedTime();
-
-    earthRef.current.rotation.y = elapsedTime / 6;
-    cloudsRef.current.rotation.y = elapsedTime / 6;
-  });
-
+  const { scene } = useGLTF("./earth3d/scene.gltf");
+  
   return (
-    <>
-      <pointLight color="#cfcfcf" position={[-1500, 350, 40]} intensity={1.8} />
-      <mesh ref={cloudsRef} position={[0, 0, 3]}>
-        <sphereGeometry args={[1.005, 32, 32]} />
-        <meshPhongMaterial
-          map={cloudsMap}
-          opacity={0.5}
-          depthWrite={true}
-          transparent={true}
-          side={THREE.DoubleSide}
-        />
-      </mesh>
-      <mesh ref={earthRef} position={[0, 0, 3]}>
-        <sphereGeometry args={[1, 32, 32]} />
-        <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial
-          map={colorMap}
-          normalMap={normalMap}
-          metalness={0.8}
-          roughness={0.7}
-        />
-
-      </mesh>
-    </>
+    <mesh>
+      <hemisphereLight intensity={0.1} groundColor='black' />
+      <spotLight
+        position={[1.100, -1, 1.450]}
+      />
+      <pointLight intensity={1} />
+      <primitive object={scene} scale={2} position-y={-0.50} position-x={-0.05} rotation-y={-1.95} />
+    </mesh>
   );
 };
 
 const EarthCanvas = () => {
+
   return (
     <Canvas
-      className="mouse-events earth"
+      className="mouse-events"
+      shadows
       frameloop='always'
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
         <Earth />
+
         <Preload all />
       </Suspense>
     </Canvas>
