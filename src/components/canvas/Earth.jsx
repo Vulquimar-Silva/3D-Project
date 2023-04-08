73cec636012b4a719ac058714cb8e76a5e6259d7
Vulquimar Silva <vuquimar@hotmail.com>
2023-04-08 12:38:47 -0300

    feat: I changed 3d model to earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -24,43 +24,42 @@ const Earth = () => {
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
 
-    earthRef.current.rotation.y = elapsedTime / 10;
-    cloudsRef.current.rotation.y = elapsedTime / 10;
+    earthRef.current.rotation.y = elapsedTime / 6;
+    cloudsRef.current.rotation.y = elapsedTime / 6;
   });
 
   return (
     <>
-    <pointLight color="#cfcfcf" position={[-1500, 350, 40]} intensity={1.8} />
-    <mesh ref={cloudsRef} position={[0, 0, 3]}>
-      <sphereGeometry args={[1.005, 32, 32]} />
-      <meshPhongMaterial
-        map={cloudsMap}
-        opacity={0.5}
-        depthWrite={true}
-        transparent={true}
-        side={THREE.DoubleSide}
-      />
-    </mesh>
-    <mesh ref={earthRef} position={[0, 0, 3]}>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshPhongMaterial specularMap={specularMap} />
-      <meshStandardMaterial
-        map={colorMap}
-        normalMap={normalMap}
-        metalness={0.8}
-        roughness={0.7}
-      />
+      <pointLight color="#cfcfcf" position={[-1500, 350, 40]} intensity={1.8} />
+      <mesh ref={cloudsRef} position={[0, 0, 3]}>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.5}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+      <mesh ref={earthRef} position={[0, 0, 3]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.8}
+          roughness={0.7}
+        />
 
-    </mesh>
-  </>
+      </mesh>
+    </>
   );
 };
 
 const EarthCanvas = () => {
   return (
     <Canvas
-      className="mouse-events"
-      shadows
+      className="mouse-events earth"
       frameloop='always'
       gl={{ preserveDrawingBuffer: true }}
     >
